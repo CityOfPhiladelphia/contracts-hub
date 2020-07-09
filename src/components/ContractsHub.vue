3de581a5eb69b2d1c1a8f3e75bda90579312de1c
m-atia <mohamed.atia@phila.gov>
2020-07-09 16:34:25 -0400

    fix titles

--- src/components/ContractsHub.vue
+++ src/components/ContractsHub.vue
@@ -498,7 +498,7 @@ export default {
             }
 
             if (contract.data_source == "PHL-Contracts") {
-              contract.display_title = contract.opportunity_description.charAt(0).toUpperCase();
+              contract.display_title = contract.opportunity_description.charAt(0).toUpperCase() +  contract.opportunity_description.slice(1);
               contract.url =
                 "https://www.phlcontracts.phila.gov/bso/external/bidDetail.sdo?bidId=" +
                 contract.bid_number +
